d4b2842de139ed2da7c9f0bf411f27ac3d662b1b
Kagiri11 <charlesmaish423@gmail.com>
2021-11-24 18:14:33 +0300

    Created function to fetch all songs

--- data/network/src/main/java/com/example/network/apis/ApiDirect.kt
+++ data/network/src/main/java/com/example/network/apis/ApiDirect.kt
@@ -1,5 +1,11 @@
 package com.example.network.apis
 
+import com.example.network.models.tracks.TracksDto
+import retrofit2.http.GET
+
 interface ApiDirect {
+
+    @GET("547789765/tracks")
+    suspend fun fetchSongs(): TracksDto
 
 }
