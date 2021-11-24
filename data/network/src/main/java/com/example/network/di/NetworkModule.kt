ec96b5bc368011efa6e619a7c3e8c65f5262761f
Kagiri11 <charlesmaish423@gmail.com>
2021-11-24 18:15:57 +0300

    Edited BASE_URL constants

--- data/network/src/main/java/com/example/network/di/NetworkModule.kt
+++ data/network/src/main/java/com/example/network/di/NetworkModule.kt
@@ -3,6 +3,7 @@ package com.example.network.di
 import com.example.network.apis.ApiDirect
 import com.example.network.apis.ApiService
 import com.example.network.utils.Constants.BASE_URL
+import com.example.network.utils.Constants.BASE_URL2
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -26,7 +27,7 @@ val networkModule= module {
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
-            .baseUrl("api.deezer.com/playlist/547789765/tracks")
+            .baseUrl(BASE_URL2)
             .build()
             .create(ApiDirect::class.java)
     }
--- data/network/src/main/java/com/example/network/utils/Constants.kt
+++ data/network/src/main/java/com/example/network/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.example.network.utils
 
 object Constants {
     const val BASE_URL = "https://api.deezer.com/"
+    const val BASE_URL2 = "api.deezer.com/playlist/"
 }
