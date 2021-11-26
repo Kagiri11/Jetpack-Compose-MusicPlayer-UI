abbec23ce7c4aab1a824dc73777ff11f63f15543
Kagiri11 <charlesmaish423@gmail.com>
2021-11-26 23:08:31 +0300

    Created mappers for TracksDto to domain tracks

--- data/repository/src/main/java/com/example/repository/di/RepositoryModule.kt
+++ data/repository/src/main/java/com/example/repository/di/RepositoryModule.kt
@@ -5,5 +5,5 @@ import com.example.repository.sources.AppRepositoryImpl
 import org.koin.dsl.module
 
 val repositoryModule = module{
-    single<AppRepository>{AppRepositoryImpl(get())}
+    single<AppRepository>{AppRepositoryImpl(get(),get())}
 }
