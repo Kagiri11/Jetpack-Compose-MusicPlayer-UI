ea6c7a50c34463f5a7851f204d85ebc5f33ec06f
Kagiri11 <charlesmaish423@gmail.com>
2021-12-01 15:01:25 +0300

    created search screen

--- app/src/main/java/com/example/lily/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/lily/ui/screens/search/SearchScreen.kt
@@ -1,17 +1,35 @@
 package com.example.lily.ui.screens.search
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lily.R
+import me.nikhilchaudhari.library.NeuInsets
+import me.nikhilchaudhari.library.neumorphic
+import me.nikhilchaudhari.library.shapes.Punched
 
 @Composable
-fun SearchScreen(modifier: Modifier  = Modifier){
-    Surface(
+fun SearchScreen(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier.fillMaxSize(),
-        color = Color.White
+        contentAlignment = Alignment.Center
     ) {
 
+
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SearchScreenPreview() {
+    SearchScreen()
 }
